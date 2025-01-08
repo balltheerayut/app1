a485fd603cc29c9c762953c0af2a9b6a18d2e86a
balltheerayut <152575191+balltheerayut@users.noreply.github.com>
2025-01-08 15:28:43 +0700

    add Event data
    add Event data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Calculate2 } from './calculator';
+import { EventData2 } from './event-data';
 function App() {
-  return  <Calculate2/>
+  return  <EventData2/>
 }
 export default App;
