4fbc2f997f5efbc5f3f3edcf6410c65c7b841d8f
Ball <ball.theerayut@gmail.com>
2025-01-15 15:54:30 +0700

    add state-func
    add state-func

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import RefsFunc from './refs-func';
+import MessageBox from './state-func';
 function App() {
-  return  <RefsFunc/>
+  return  <MessageBox/>
 }
 export default App;
